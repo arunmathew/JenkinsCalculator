b2c22d4b1ac28901ba96ec9708de44bd8e6a7e72
arunmathew <arunmathew1712@gmail.com>
2020-05-10 12:24:36 -0400

    First Commit

---
+++ src/main/java/com/arun/jenkins/JenkinsCalc.java
@@ -0,0 +1,11 @@
+package com.arun.jenkins;
+
+public class JenkinsCalc {
+    public int addNum(int a , int b){
+        return a+b;
+    }
+    public int subNum(int a, int b){
+        return a-b;
+        }
+}
+
---
+++ src/test/java/com/arun/jenkins/JenkinsCalcTest.java
@@ -0,0 +1,19 @@
+package com.arun.jenkins;
+
+import static org.junit.Assert.*;
+
+public class JenkinsCalcTest {
+
+    @org.junit.Test
+    public void addNum() {
+        JenkinsCalc myCalc = new JenkinsCalc();
+        assertEquals(10, myCalc.addNum(7,3));
+
+    }
+
+    @org.junit.Test
+    public void subNum() {
+        JenkinsCalc myCalc = new JenkinsCalc();
+        assertEquals(5, myCalc.subNum(8,3));
+    }
+}
